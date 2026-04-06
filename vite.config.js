ffe58951eb4bab79427430d3eaa93a7daad0157b
abhi19152310 <abhishekdevara01@gmail.com>
2026-04-06 19:19:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,2 +1,9 @@
-import { defineConfig } from 'vite';
-export default defineConfig({ root: '.', publicDir: 'public' });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  root: '.', 
+  publicDir: 'public',
+  plugins: [react()],
+  base: '/book-explorer/'   // 🔥 important for GitHub Pages
+})
